55b3f802d1378f1d7e761e55ea59c93538b61a9c
sjoseph1848 <sjoseph1848@gmail.com>
2020-09-06 12:59:55 -0500

    added the news section on the home page

--- src/components/section/Info.js
+++ src/components/section/Info.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
+
 const Info = () => {
     return (
-        <section className="info">
-            Info Works!
+        <section id="info">
+            <header className="info-head">
+                <h2>News on the Markets</h2>
+                <h3>The place to get all the news about your stocks.</h3>
+            </header>
         </section>
     )
 }
