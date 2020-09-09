d6dddd8a81fbf8c7dda2f836cbc06254dd36dfcd
sjoseph1848 <sjoseph1848@gmail.com>
2020-09-08 21:05:27 -0500

    updated the news page and added favicon and SEO

--- src/components/pages/News.js
+++ src/components/pages/News.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios';
 import NewsCard from '../section/NewsCard';
-
+import './News.css'
 class News extends Component {
     state = {
         newsCards: [],
@@ -9,16 +9,18 @@ class News extends Component {
     }
     async componentDidMount() {
         this.setState({ loading: true })
-        const res = await axios.get('https://roundball-magic.azurewebsites.net/api/news/today');
+        const res = await axios.get('https://nottingham-1848-slayer-robin.azurewebsites.net/api/news/business');
         console.log(res.data);
         this.setState({ newsCards: res.data, loading: false })
     }
     render() {
         return (
             <section id="news">
-                {this.state.newsCards.map((card, i) => (
-                    <NewsCard key={i} card={card}> </NewsCard>
-                ))}
+                <div className="news-cards">
+                    {this.state.newsCards.map((card, i) => (
+                        <NewsCard key={i} card={card}> </NewsCard>
+                    ))}
+                </div>
             </section>
         )
     }
--- src/components/section/NewsCard.js
+++ src/components/section/NewsCard.js
@@ -3,24 +3,16 @@ import './NewsCard.css'
 const NewsCard = ({ card: { title, urlToImage, url } }) => {
 
     return (
-        // <div>
-        //     <p>{title}</p>
-        //     <p>{urlToImage}</p>
-        //     <a href={url} style={{ color: 'black' }} rel="noopener noreferrer" target="_blank">Read More</a>
-        // </div>
 
-        <div className="center">
-            <div className="property-card">
-                <a href={url} target="_blank" rel="noopener noreferrer">
-                    <div className="property-image">
-                        <img src={urlToImage} alt="News Article API Image" />
-                    </div></a>
-                <div className="property-description">
-                    <h5>{title}</h5>
-                </div>
+        <div className="property-card">
+            <a href={url} target="_blank" rel="noopener noreferrer">
+                <div className="property-image">
+                    <img src={urlToImage} alt="News Article" />
+                </div></a>
+            <div className="property-description">
+                <h5>{title}</h5>
             </div>
         </div>
-
     )
 }
 
