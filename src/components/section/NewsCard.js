a36414f008e2d0bef83eb83a34dbb10b4cb18e4f
sjoseph1848 <sjoseph1848@gmail.com>
2020-09-08 19:10:39 -0500

    added the news card

--- src/components/section/NewsCard.js
+++ src/components/section/NewsCard.js
@@ -1,13 +1,26 @@
 import React from 'react';
-
+import './NewsCard.css'
 const NewsCard = ({ card: { title, urlToImage, url } }) => {
 
     return (
-        <div>
-            <p>{title}</p>
-            <p>{urlToImage}</p>
-            <a href={url} style={{ color: 'black' }} rel="noopener noreferrer" target="_blank">Read More</a>
+        // <div>
+        //     <p>{title}</p>
+        //     <p>{urlToImage}</p>
+        //     <a href={url} style={{ color: 'black' }} rel="noopener noreferrer" target="_blank">Read More</a>
+        // </div>
+
+        <div className="center">
+            <div className="property-card">
+                <a href={url} target="_blank" rel="noopener noreferrer">
+                    <div className="property-image">
+                        <img src={urlToImage} alt="News Article API Image" />
+                    </div></a>
+                <div className="property-description">
+                    <h5>{title}</h5>
+                </div>
+            </div>
         </div>
+
     )
 }
 
